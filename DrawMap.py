aa02767ec3aba44a5593847439503deb6c031724
ntkhoang <142283277+ntkhoang@users.noreply.github.com>
2024-03-23 15:22:51 +0700

    Add obstacle drawing functionality to DrawMap.py

--- DrawMap.py
+++ DrawMap.py
@@ -5,10 +5,14 @@ class MAP:
     def __init__(self,filename, windows):
         self.player = None
         self.map_data = []
+        self.obstacles = list()
         with open(filename, 'r') as file:
             rows, cols = map(int, file.readline().split()) 
+            for _ in range(rows):
+                self.map_data.append(list(file.readline().strip()))
             for line in file:
-                self.map_data.append(list(line.strip()))
+                for number in line.split():
+                    self.obstacles.append(int(number))
         self.win = windows
         self.tile_size = min(self.win.get_width() // cols, self.win.get_height() // rows)
 
@@ -23,3 +27,12 @@ class MAP:
                     pygame.draw.rect(self.win, (0, 0, 255), (j*self.tile_size, i*self.tile_size, self.tile_size, self.tile_size))
                 elif col == '3':
                     pygame.draw.rect(self.win, (255, 0, 0), (j*self.tile_size, i*self.tile_size, self.tile_size, self.tile_size))
+
+        top = self.obstacles[0] * self.tile_size
+        left = self.obstacles[1] * self.tile_size
+        down = self.obstacles[2] * self.tile_size
+        right = self.obstacles[3] * self.tile_size  
+        for top in range (down):
+            pygame.draw.rect(self.win, (255, 255, 0), (left, top, self.tile_size, self.tile_size))
+        for left in range (right):
+            pygame.draw.rect(self.win, (255, 255, 0), (left, top, self.tile_size, self.tile_size))          
