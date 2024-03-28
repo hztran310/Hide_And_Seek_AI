223c30f2ebf1e9a48b190cbb41ffe847e0f460f3
aimbs <lanthaotran310@gmail.com>
2024-03-28 09:47:22 +0700

    append move into move_data

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ m = MAP('Map/Map1.txt', win)
 # Create the characters
 seeker = Seeker(m, win)
 hider = Hider(m, win)
-num_hiders = 1
+num_hiders = 2
+
 
 # Set the initial positions of the characters
 seeker.set_position()
@@ -37,8 +38,6 @@ for i in range(0, len(list_obstacles), 4):
     obs = Obstacle(list_obstacles[i], list_obstacles[i+1], list_obstacles[i+2], list_obstacles[i+3], m, win)
     obstacles.append(obs)
 
-list = [5, 6, 7, 8, 9, 10]
-random_move = random.choice(list)
 
 while running:
     clock.tick(FPS)
@@ -62,24 +61,39 @@ while running:
             seeker.remove_obstacle()
         
     if seeker.obstacle is None:
-        seeker.move()
+        if len(seeker.move_data) == 0 and seeker.has_append_move == True:
+            break
+        seeker_move = seeker.random_move()
+        if seeker_move == 'Up':
+            seeker.move_up()
+        elif seeker_move == 'Down':
+            seeker.move_down()
+        elif seeker_move == 'Left':
+            seeker.move_left()
+        elif seeker_move == 'Right':
+            seeker.move_right()
         seeker.character_vision(3)
-    else:
-        seeker.obstacle.remove_draw()
-        seeker.move_obstacle()
-        seeker.obstacle.draw()
-        seeker.character_vision(3)
-
+        seeker.move_data.pop(0)
+    # else:
+    #     seeker.obstacle.remove_draw()
+    #     seeker.move_obstacle()
+    #     seeker.obstacle.draw()
+    #     seeker.character_vision(3)
+    
+    
+    
+    #Draw seeker
     pygame.draw.rect(win, (255, 0, 0), (seeker.col * m.tile_size, seeker.row * m.tile_size, m.tile_size, m.tile_size))
 
     SCORE_TEXT = SCORE_FONT.render(f'Score: {seeker.score}', True, (0, 0, 0))  # Create a text surface with the score
     win.blit(SCORE_TEXT, [0,0])   
     
+    #Draw hider
     pygame.draw.rect(win, (0, 0, 255), (hider.col * m.tile_size, hider.row * m.tile_size, m.tile_size, m.tile_size))
     
     # Update the display
     pygame.display.update()
-    
+
     if seeker.found_hider(hider):
         win.fill((133, 151, 153), pygame.Rect(0, 0, SCORE_TEXT.get_width(), SCORE_TEXT.get_height()))  # Fill the area with white color
         pygame.display.update()
@@ -90,7 +104,6 @@ while running:
         pygame.time.wait(3000)  # Wait for 3 seconds
         break
     
-
     if seeker.move_count == random_move:
         random_move = random.choice(list)
         hider_location = []
@@ -109,6 +122,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         
+    pygame.time.delay(1000)  # Adjust the delay time as needed
 
 
 pygame.quit()
