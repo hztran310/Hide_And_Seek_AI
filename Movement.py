0a9a76ada9f631ba7a485eac938b725878866446
ntkhoang <142283277+ntkhoang@users.noreply.github.com>
2024-04-03 16:31:36 +0700

    Fix hider removal bug in Movement.py

--- Movement.py
+++ Movement.py
@@ -173,6 +173,10 @@ def movement(num_hiders, filename):
                         seeker.target_location = None
                         seeker.set_target_location((hider.row, hider.col))
                         seeker.hider_location = (hider.row, hider.col)
+                    else:
+                        for cell_list in may_be_hider:
+                            if cell in cell_list:
+                                cell_list.remove(cell)
         
         for hider in hiders:
             pygame.draw.rect(win, COLOR_HIDER, (hider.col * m.tile_size, hider.row * m.tile_size, m.tile_size, m.tile_size))
